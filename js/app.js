4335533bfc3738db0a4fea792602e513bb925d35
chloereynolds07 <chloe@codingacademies.org>
2024-12-06 12:01:47 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const switchBtn = document.getElementById('switchBtn')
+const switchBtn2 = document.getElementById('switchBtn2')
+
+const light = document.getElementById('light')
+
+
+switchBtn.addEventListener('click', ()=> {
+    console.log('click')
+})
+
+switchBtn2.addEventListener('click', ()=> {
+    console.log('click')
+})
+
+
+function changeLight() {
+}
